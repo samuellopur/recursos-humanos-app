8b4cf8426ce393d703df4b704bc67a5f8a3f1c72
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-26 21:21:05 -0500

    Componente de navegacion con Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
 
 function App() {
   return (
-    <ListadoEmpleados/>
+    <div className="container">
+      <Navegacion/>
+      <ListadoEmpleados/>
+    </div>
+    
   );
 }
 
