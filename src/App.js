1679be6cc05158343034f3ebbfdafdfc47fb1eb5
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-27 20:37:08 -0500

    Componente de rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
 import Navegacion from "./plantilla/Navegacion";
 
 function App() {
   return (
     <div className="container">
+      <BrowserRouter>
       <Navegacion/>
-      <ListadoEmpleados/>
+      <Routes>
+        <Route exact path="/" element={<ListadoEmpleados/>}/>
+      </Routes>
+      
+      </BrowserRouter>
     </div>
     
   );
