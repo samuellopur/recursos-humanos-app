86842c34c06767561107becff2841a7c9a7e0bd7
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-30 21:12:16 -0500

    Componente de Editar

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { NumericFormat } from "react-number-format";
+import { Link } from "react-router-dom";
 
 export default function ListadoEmpleados() {
   const urlBase = "http://localhost:8080/rh-app/empleados";
@@ -31,6 +32,7 @@ export default function ListadoEmpleados() {
             <th scope="col">Empleado</th>
             <th scope="col">Departamento</th>
             <th scope="col">Sueldo</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -50,7 +52,15 @@ export default function ListadoEmpleados() {
                     decimalScale={2}
                     fixedDecimalScale
                   />
-                </td>
+                  </td>
+                  <td className="text-center">
+                    <div>
+                      <Link to={`/editar/${empleado.idEmpleado}`}
+                      className='btn btn-warning btn-sm me-3'>Editar</Link>
+                    </div>
+
+                  </td>
+                
               </tr>
             ))
           }
